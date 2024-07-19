9893db99c9ea35e8491b9481fdaa5e0622d25a32
“Janvi” <limbasiyajanvi@email.com>
2024-07-19 18:48:43 +0530

    sidebar Ui update

--- gsap/src/Components/Global/Sidebar/Sidebar.jsx
+++ gsap/src/Components/Global/Sidebar/Sidebar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import GlobalSvg from "../../../assets/Icons/GlobalSvg";
+import { GoProjectRoadmap } from "react-icons/go";
+import { GrAnalytics } from "react-icons/gr";
+import { FaMoneyBill } from "react-icons/fa";
+import { LuCircleDollarSign } from "react-icons/lu";
+import { HiOutlineAcademicCap } from "react-icons/hi";
+import { FaRegCalendar } from "react-icons/fa";
+import { MdOutlineMessage } from "react-icons/md";
+import { IoMdContacts } from "react-icons/io";
 
 function Sidebar() {
 	return (
@@ -18,30 +26,142 @@ function Sidebar() {
 							alt='react'
 							width='16'
 						></img>
-						<span>React</span>{" "}
+						<span>React</span>
 					</div>
 				</section>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					width='20'
-					height='20'
-					viewBox='0 0 300 150'
-					style={{ fill: "#FFFFFF" }}
-				>
-					<g
-						fill='#ffffff'
-						fillRule='nonzero'
-						stroke='none'
-						strokeWidth='1'
-						strokeLinecap='butt'
-						strokeLinejoin='miter'
-						strokeMiterlimit='10'
+				<section className='toggle-container'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						width='20'
+						height='20'
+						viewBox='0 0 300 150'
+						style={{ fill: "#FFFFFF" }}
 					>
-						<g transform='scale(5.12,5.12)'>
-							<path d='M5,8c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175zM5,23c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175zM5,38c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175z' />
+						<g
+							fill='#ffffff'
+							fillRule='nonzero'
+							stroke='none'
+							strokeWidth='1'
+							strokeLinecap='butt'
+							strokeLinejoin='miter'
+							strokeMiterlimit='10'
+						>
+							<g transform='scale(5.12,5.12)'>
+								<path d='M5,8c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175zM5,23c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175zM5,38c-0.72127,-0.0102 -1.39216,0.36875 -1.75578,0.99175c-0.36361,0.623 -0.36361,1.39351 0,2.01651c0.36361,0.623 1.0345,1.00195 1.75578,0.99175h40c0.72127,0.0102 1.39216,-0.36875 1.75578,-0.99175c0.36361,-0.623 0.36361,-1.39351 0,-2.01651c-0.36361,-0.623 -1.0345,-1.00195 -1.75578,-0.99175z' />
+							</g>
 						</g>
-					</g>
-				</svg>
+					</svg>
+				</section>
+			</article>
+			<article className='profile-container font-style'>
+				<img
+					src='https://react-material.fusetheme.com/assets/images/avatars/brian-hughes.jpg'
+					className='profile-picture'
+				/>
+				<section className='profile-info-container'>
+					<span className='profile-font'>Abbott Keitch</span>
+					<span className='email-font'>admin@fusetheme.com</span>
+				</section>
+			</article>
+			<article className='menu-container'>
+				<div className='menu-heading-container'>
+					<span className='main-heading'>DASHBOARDS</span>
+					<span className='sub-heading '>
+						Unique dashboard designs
+					</span>
+				</div>
+				<div className='menu-list'>
+					<div className='menu-list-style'>
+						<GoProjectRoadmap
+							style={{
+								color: "#FFFFFFB3",
+								height: "20px",
+								width: "20px",
+							}}
+						/>
+						<div className='menu-name'>Project</div>
+					</div>
+					<div className='menu-list-style'>
+						<GrAnalytics
+							style={{
+								color: "#FFFFFFB3",
+								height: "20px",
+								width: "20px",
+							}}
+						/>
+						<div className='menu-name'>Analytics</div>
+					</div>
+					<div className='menu-list-style'>
+						<FaMoneyBill
+							style={{
+								color: "#FFFFFFB3",
+								height: "20px",
+								width: "20px",
+							}}
+						/>
+						<div className='menu-name'>Finance</div>
+					</div>
+					<div className='menu-list-style'>
+						<LuCircleDollarSign
+							style={{
+								color: "#FFFFFFB3",
+								height: "20px",
+								width: "20px",
+							}}
+						/>
+						<div className='menu-name'>Crypto</div>
+					</div>
+				</div>
+				<div className='menu-heading-container'>
+					<span className='main-heading'>APPLICATIONS</span>
+					<span className='sub-heading '>
+						Custom made application designs
+					</span>
+				</div>
+				<div className='menu-list'>
+					<div className='menu-list-style'>
+						<HiOutlineAcademicCap
+							style={{
+								color: "#FFFFFFB3",
+								height: "22px",
+								width: "22px",
+							}}
+						/>
+						<div className='menu-name'>Academy</div>
+					</div>
+					<div className='menu-list-style'>
+						<FaRegCalendar
+							style={{
+								color: "#FFFFFFB3",
+								height: "20px",
+								width: "20px",
+							}}
+						/>
+						<div className='menu-name'>Calendar</div>
+					</div>
+					<div className='menu-list-style'>
+						<MdOutlineMessage
+							style={{
+								color: "#FFFFFFB3",
+								height: "20px",
+								width: "20px",
+							}}
+						/>
+						<div className='menu-name'>Message</div>
+					</div>
+					<div className='menu-list-style'>
+						<IoMdContacts
+							style={{
+								color: "#FFFFFFB3",
+								height: "20px",
+								width: "20px",
+							}}
+						/>
+						<div className='menu-name'>Contacts</div>
+					</div>
+				</div>
+
+				<section></section>
 			</article>
 		</div>
 	);
